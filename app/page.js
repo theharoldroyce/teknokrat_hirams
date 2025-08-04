bb7c3ee1fa57bb178e304aae4dc485830ec6ad74
DESKTOP-P7OHTDI\theha <theharoldroyce@yahoo.com>
2025-08-04 09:39:12 +0800

    add login

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import LoginPage from "./login/page";
 
 export default function Home() {
   return (
     <div>
-      
+      <LoginPage />
     </div>
   );
 }
