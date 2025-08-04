731798a9491f0f3024df9c9d100a12524f6ce404
DESKTOP-P7OHTDI\theha <theharoldroyce@yahoo.com>
2025-08-04 10:06:27 +0800

    hide login in page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
     <div
       className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm />
+       {/* <LoginForm /> */}
       </div>
     </div>
   );
